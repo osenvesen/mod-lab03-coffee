be26f7a1aeebece5df3a025e4d65f62c743d6518
osenvesen <147384976+osenvesen@users.noreply.github.com>
2024-05-10 16:16:10 +0300

    Create Automata.cpp

---
+++ src/Automata.cpp
@@ -0,0 +1,94 @@
+#include "Automata.h"
+#include <iostream>
+
+Automata::Automata() {
+cash = 0;
+menu[0] = "Tea";
+menu[1] = "Coffee";
+menu[2] = "Milk";
+prices[0] = 10;
+prices[1] = 15;
+prices[2] = 20;
+state = OFF;
+}
+
+void Automata::on() {
+if (state == OFF) {
+state = WAIT;
+std::cout « "Automata is turned on.\n";
+}
+}
+
+void Automata::off() {
+if (state == WAIT || state == OFF) {
+state = OFF;
+std::cout « "Automata is turned off.\n";
+}
+}
+
+void Automata::coin(int sum) {
+if (state == ACCEPT || state == WAIT) {
+cash += sum;
+state = ACCEPT;
+std::cout « "Cash: " « cash « " rubles.\n";
+}
+}
+
+void Automata::getMenu() {
+if (state == WAIT) {
+std::cout « "Menu:\n";
+for (int i = 0; i < 3; i++) {
+std::cout « i+1 « ". " « menu[i] « " - " « prices[i] « " rubles.\n";
+}
+std::cout « "Choose a drink.\n";
+}
+}
+
+STATES Automata::getState() {
+return state;
+}
+
+void Automata::choice(int drink) {
+if (state == ACCEPT) {
+if (drink >= 1 && drink <= 3) {
+int cost = prices[drink-1];
+if (check(cost)) {
+state = COOK;
+cook();
+} else {
+std::cout « "Not enough cash.\n";
+}
+}
+}
+}
+
+bool Automata::check(int cost) {
+if (cash >= cost) {
+cash -= cost;
+return true;
+}
+return false;
+}
+
+void Automata::cancel() {
+if (state == ACCEPT) {
+cash = 0;
+state = WAIT;
+std::cout « "Canceled.\n";
+}
+}
+
+void Automata::cook() {
+if (state == COOK) {
+std::cout « "Cooking...\n";
+std::cout « "Done! Enjoy your drink!\n";
+state = WAIT;
+}
+}
+
+void Automata::finish() {
+if (state == WAIT) {
+cash = 0;
+std::cout « "Finished.\n";
+}
+}
